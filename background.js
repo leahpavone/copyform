af5a30ea30139dbbadd19204a1400bcf1952c2e5
Leah Pavone <leah@Ls-MacBook-Pro.socal.rr.com>
2022-07-12 17:11:58 -0700

    first commit

---
+++ background.js
@@ -0,0 +1,60 @@
+function ctxMenuOnClick(info, tab) {
+  if (info.menuItemId == "copy_form") {
+    chrome.tabs.sendMessage(tab.id, { copyform: true }, function (response) {
+      if (response) {
+        chrome.storage.local.set(
+          { clipboard: JSON.stringify(response) },
+          function () {
+            console.log("Copied form successfully");
+            chrome.contextMenus.update("paste_form", { enabled: true });
+            chrome.contextMenus.update("clear_form_clipboard", {
+              enabled: true
+            });
+          }
+        );
+      } else {
+        console.log(
+          "Sorry, you have to refresh the page before it will work when first installed."
+        );
+        alert(
+          "Sorry, you have to refresh the page before it will work when first installed."
+        );
+      }
+    });
+  } else if (info.menuItemId == "paste_form") {
+    chrome.storage.local.get("clipboard", function (obj) {
+      var lastFormData = JSON.parse(obj.clipboard);
+      lastFormData.pasteform = true;
+      chrome.tabs.sendMessage(tab.id, lastFormData, function (response) {
+        console.log("Pasted form successfully");
+      });
+    });
+  } else if (info.menuItemId == "clear_form_clipboard") {
+    chrome.storage.local.set({ clipboard: "{}" }, function () {
+      chrome.contextMenus.update("paste_form", { enabled: false });
+      chrome.contextMenus.update("clear_form_clipboard", { enabled: false });
+    });
+  }
+}
+
+chrome.contextMenus.onClicked.addListener(ctxMenuOnClick);
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: "Copy Form",
+    id: "copy_form",
+    contexts: ["editable"]
+  });
+  chrome.contextMenus.create({
+    title: "Paste Form",
+    id: "paste_form",
+    contexts: ["editable"],
+    enabled: false
+  });
+  chrome.contextMenus.create({
+    title: "Clear Form Clipboard",
+    id: "clear_form_clipboard",
+    contexts: ["editable"],
+    enabled: false
+  });
+});
---
+++ copyform.user.js
@@ -0,0 +1,59 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  var active = document.activeElement;
+  var parentForm = active.parentNode || active;
+  while (parentForm.nodeName != "FORM") {
+    parentForm = parentForm.parentNode;
+  }
+  if (request.pasteform) {
+    delete request.pasteform;
+    for (i in request) {
+      var vals = request[i];
+      var valsIsArray = typeof vals == "object";
+      var elms = parentForm.querySelectorAll("[name='" + i + "']"),
+        n,
+        v;
+      for (var frmelm = 0, l = elms.length; frmelm < l; frmelm++) {
+        n = elms[frmelm];
+        v = valsIsArray ? vals[frmelm] : vals;
+        typeof v;
+        if (n) {
+          if (n.type == "checkbox") {
+            n.checked = v;
+          } else n.value = v;
+        }
+      }
+    }
+    sendResponse({});
+  } else if (request.copyform) {
+    var resp = {};
+    resp = appendFrmElms(resp, parentForm.getElementsByTagName("input"));
+    resp = appendFrmElms(resp, parentForm.getElementsByTagName("select"));
+    resp = appendFrmElms(resp, parentForm.getElementsByTagName("textarea"));
+    sendResponse(resp);
+  }
+});
+
+function appendFrmElms(resp, formElms) {
+  for (var f = 0, l = formElms.length; f < l; f++) {
+    var newValue = null;
+    if (formElms[f].type == "hidden") {
+      //skip
+    } else if (formElms[f].type == "checkbox") {
+      newValue = formElms[f].checked;
+    } else {
+      newValue = formElms[f].value;
+    }
+
+    if (newValue !== null) {
+      if (resp[formElms[f].name]) {
+        if (typeof resp[formElms[f].name] != "object") {
+          resp[formElms[f].name] = [resp[formElms[f].name]];
+        }
+        resp[formElms[f].name].push(newValue);
+      } else {
+        resp[formElms[f].name] = newValue;
+      }
+    }
+  }
+  return resp;
+}
